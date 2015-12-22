061d81c0c3e6522443f9f6bc71931657f06d08f1
Matthew Ecker <gutlessVADER@gmail.com>
2015-12-22 06:56:29 -0600

    added files

---
+++ Gw2-64-affinity.bat
@@ -0,0 +1,2 @@
+start /affinity 55 Gw2-64.exe -mapLoadInfo -autologin
+exit
---
+++ Source.cpp
@@ -0,0 +1,9 @@
+#include "windows.h"
+
+using namespace std;
+
+int main()
+{
+	system("GW2-64-affinity.bat");
+	return 0;
+}
