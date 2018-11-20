5e87dd2091123bdca1045e9fe31add0c2506c3fe
Keith Miller <keith.miller@thermosystems.com>
2018-11-20 11:19:58 -0500

    Update Source.cpp
    Add comments to make awful code readable

--- Source.cpp
+++ Source.cpp
@@ -1,9 +1,23 @@
+/*
+ * main.cpp
+ *
+ * run the GW2-64-affinity batch file included in the same
+ * directory as this compiled executable.
+ */
+
+//include the windows header file
 #include "windows.h"
 
+//using the standard namespace (lol)
 using namespace std;
 
 int main()
 {
+	//utilize the Windows Shell to run the GW2-64-affinity batch file
 	system("GW2-64-affinity.bat");
+	
+	//return 0 to the Windows Shell upon succccccc completion
 	return 0;
 }
+
+//WABALDUBADUBDUBBBB!!!!!!!!!!!!!! IM PICKLE RIIIIIIIIIIIIIIIICK
